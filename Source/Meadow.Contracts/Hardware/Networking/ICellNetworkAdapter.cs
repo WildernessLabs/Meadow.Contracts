70f87bca2967baa1ec2b5c517427d2cd9896f38c
Chris Tacke <ctacke@opennetcf.com>
2023-07-27 11:09:51 -0500

    Added CellNetwork for cell scanning

--- Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
+++ Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
@@ -1,6 +1,12 @@
-﻿namespace Meadow.Hardware
+﻿using Meadow.Networking;
+
+namespace Meadow.Hardware
 {
     public interface ICellNetworkAdapter : INetworkAdapter
     {
+        /// <summary>
+        /// Returns a list of cellular networks
+        /// </summary>
+        CellNetwork[]? Scan();
     }
 }
