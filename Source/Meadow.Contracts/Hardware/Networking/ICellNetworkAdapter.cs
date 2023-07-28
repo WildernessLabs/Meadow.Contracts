35932d469a757dcedc5baf73fc2bf4a00862c516
halyssonJr <halysson1007@gmail.com>
2023-07-28 16:59:30 -0300

    add Cell Scanning Network

--- Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
+++ Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
@@ -1,6 +1,16 @@
-﻿namespace Meadow.Hardware
+﻿using System.Collections.Generic;
+
+namespace Meadow.Hardware
 {
+    public class CellOperator
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public string Operator { get; set; }
+        public string Code { get; set; }
+    }
     public interface ICellNetworkAdapter : INetworkAdapter
     {
+        public List<CellOperator> ScanNetwork ();       
     }
 }
