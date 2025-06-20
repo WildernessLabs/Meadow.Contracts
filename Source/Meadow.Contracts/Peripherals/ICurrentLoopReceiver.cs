7a08a1f8f4c8ff3d1eb4f2f611fbb72ded412d33
Chris Tacke <ctacke@opennetcf.com>
2025-06-19 22:26:39 -0500

    fixing current sensor interfaces

---
+++ Source/Meadow.Contracts/Peripherals/ICurrentLoopReceiver.cs
@@ -0,0 +1,16 @@
+﻿using Meadow.Units;
+using System.Threading.Tasks;
+
+namespace Meadow.Hardware;
+
+/// <summary>
+/// Interface for receiving current loop signals from industrial sensors
+/// </summary>
+public interface ICurrentLoopReceiver
+{
+    /// <summary>
+    /// Gets the input current for the receiver
+    /// </summary>
+    /// <returns>A task representing the asynchronous operation</returns>
+    Task<Current> GetInputCurrent();
+}
---
+++ Source/Meadow.Contracts/Peripherals/Sensors/Electrical/ISamplingCurrentSensor.cs
@@ -0,0 +1,10 @@
+﻿using Meadow.Units;
+
+namespace Meadow.Peripherals.Sensors;
+
+/// <summary>
+/// Electrical Current sensor interface requirements.
+/// </summary>
+public interface ISamplingCurrentSensor : ISamplingSensor<Current>, ICurrentSensor
+{
+}
