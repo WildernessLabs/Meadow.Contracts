9aae6c438f0546410f2ff634d38f05ad69ecc921
Halysson <halysson1007@gmail.com>
2025-08-11 16:34:44 -0300

    add modem types

---
+++ Source/Meadow.Contracts/Enums/CellModemType.cs
@@ -0,0 +1,20 @@
+namespace Meadow;
+
+/// <summary>
+/// Type of Cell Modem.
+/// </summary>
+public enum CellModemType
+{
+    /// <summary>
+    /// LTE cell modem BG95 manufactured by Quectel
+    /// </summary>
+    Bg95,
+    /// <summary>
+    /// GSM cell modem M95 manufactured by Quectel
+    /// </summary>
+    M95,
+    /// <summary>
+    /// LTE cell modem EG21BL manufactured by Quectel
+    /// </summary>
+    Eg21gl,
+};
