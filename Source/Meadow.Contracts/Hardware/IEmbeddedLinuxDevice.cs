58441fbdae3fa525f78894a8baffc4be97838fa5
Chris Tacke <ctacke@gmail.com>
2026-05-25 13:06:01 -0500

    feat: add a couple interfaces for linux

---
+++ Source/Meadow.Contracts/Hardware/IEmbeddedLinuxDevice.cs
@@ -0,0 +1,9 @@
+﻿namespace Meadow;
+
+/// <summary>
+/// Defines a contract for embedded Linux devices, specifying the operations and behaviors that implementing classes
+/// must provide.
+/// </summary>
+/// <remarks>Implementations of this interface should provide device-specific functionality for interacting with
+/// embedded Linux hardware. This enables consistent interaction across different device types and platforms.</remarks>
+public interface IEmbeddedLinuxDevice { }
